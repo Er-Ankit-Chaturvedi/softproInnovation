2a8e048926a4bbfda63cd018e66dad59c770fd2f
ankur yadav <ankury2002@gmail.com>
2026-09-15 16:36:17 +0530

    dns updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-
+const dns = require('dns')
+dns.setDefaultResultOrder('ipv4first');
 const mongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
